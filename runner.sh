65c836f06d287e225c68c6cf2be581031d2983b5
stevanusc6 <stevanusc6@hotmail.co.id>
2021-10-29 22:16:38 +0700

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
 # Initialize airflow environment
+rm -rf dags
+rm -rf logs
+rm -rf plugins
+rm -rf postgres
+
+mkdir dags
+mkdir logs
+mkdir plugins
+mkdir postgres
+
+chmod -R 775 dags
+chmod -R 775 logs
+chmod -R 775 plugins
+chmod -R 775  postgres
+
 docker-compose up airflow-init
 docker-compose up airflow-connection-add-source
 docker-compose up airflow-connection-add-target
@@ -10,5 +25,6 @@ cp dag_trf_data.py dags
 docker-compose down airflow-connection-add-source
 docker-compose down airflow-connection-add-target
 docker-compose down airflow-init
+
 
 docker-compose up -d
